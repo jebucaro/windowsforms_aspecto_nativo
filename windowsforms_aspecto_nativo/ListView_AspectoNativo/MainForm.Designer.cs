0d2a79567bad503581d15efdd98100854b9c1813
Jonathan Búcaro <hola@jonathanbucaro.com>
2019-12-04 21:34:07 -0600

    NativeListView

---
+++ windowsforms_aspecto_nativo/ListView_AspectoNativo/MainForm.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ListView_AspectoNativo
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.nativeListView1 = new ListView_AspectoNativo.NativeListView();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.imageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(360, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // nativeListView1
+            // 
+            this.nativeListView1.HideSelection = false;
+            this.nativeListView1.LargeImageList = this.imageList2;
+            this.nativeListView1.Location = new System.Drawing.Point(12, 39);
+            this.nativeListView1.Name = "nativeListView1";
+            this.nativeListView1.Size = new System.Drawing.Size(360, 310);
+            this.nativeListView1.SmallImageList = this.imageList1;
+            this.nativeListView1.TabIndex = 1;
+            this.nativeListView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "9gag-icon.png");
+            this.imageList1.Images.SetKeyName(1, "500px-icon.png");
+            this.imageList1.Images.SetKeyName(2, "evernote-icon.png");
+            this.imageList1.Images.SetKeyName(3, "facebook-messenger-icon.png");
+            this.imageList1.Images.SetKeyName(4, "fb-icon.png");
+            this.imageList1.Images.SetKeyName(5, "feedly-icon.png");
+            this.imageList1.Images.SetKeyName(6, "firefox-icon.png");
+            this.imageList1.Images.SetKeyName(7, "flipboard-icon.png");
+            this.imageList1.Images.SetKeyName(8, "google-icon.png");
+            this.imageList1.Images.SetKeyName(9, "google-plus-icon.png");
+            this.imageList1.Images.SetKeyName(10, "ie-icon.png");
+            this.imageList1.Images.SetKeyName(11, "ingress-icon.png");
+            this.imageList1.Images.SetKeyName(12, "instagram-icon.png");
+            this.imageList1.Images.SetKeyName(13, "office-2013-icon.png");
+            this.imageList1.Images.SetKeyName(14, "opera-icon.png");
+            this.imageList1.Images.SetKeyName(15, "pinterest-icon.png");
+            this.imageList1.Images.SetKeyName(16, "pocket-icon.png");
+            this.imageList1.Images.SetKeyName(17, "skydrive-icon.png");
+            this.imageList1.Images.SetKeyName(18, "twitter-icon.png");
+            // 
+            // imageList2
+            // 
+            this.imageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList2.ImageStream")));
+            this.imageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList2.Images.SetKeyName(0, "9gag-icon.png");
+            this.imageList2.Images.SetKeyName(1, "500px-icon.png");
+            this.imageList2.Images.SetKeyName(2, "evernote-icon.png");
+            this.imageList2.Images.SetKeyName(3, "facebook-messenger-icon.png");
+            this.imageList2.Images.SetKeyName(4, "fb-icon.png");
+            this.imageList2.Images.SetKeyName(5, "feedly-icon.png");
+            this.imageList2.Images.SetKeyName(6, "firefox-icon.png");
+            this.imageList2.Images.SetKeyName(7, "flipboard-icon.png");
+            this.imageList2.Images.SetKeyName(8, "google-icon.png");
+            this.imageList2.Images.SetKeyName(9, "google-plus-icon.png");
+            this.imageList2.Images.SetKeyName(10, "ie-icon.png");
+            this.imageList2.Images.SetKeyName(11, "ingress-icon.png");
+            this.imageList2.Images.SetKeyName(12, "instagram-icon.png");
+            this.imageList2.Images.SetKeyName(13, "office-2013-icon.png");
+            this.imageList2.Images.SetKeyName(14, "opera-icon.png");
+            this.imageList2.Images.SetKeyName(15, "pinterest-icon.png");
+            this.imageList2.Images.SetKeyName(16, "pocket-icon.png");
+            this.imageList2.Images.SetKeyName(17, "skydrive-icon.png");
+            this.imageList2.Images.SetKeyName(18, "twitter-icon.png");
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.nativeListView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "MainForm";
+            this.Text = "Native ListView";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private NativeListView nativeListView1;
+        private System.Windows.Forms.ImageList imageList2;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ windowsforms_aspecto_nativo/ListView_AspectoNativo/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListView_AspectoNativo
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            // use Segoe UI in Vista & 7
+            Font = SystemFonts.MessageBoxFont;
+
+            InitializeComponent();
+
+            comboBox1.DataSource = Enum.GetValues(typeof(View));
+
+            nativeListView1.BeginUpdate();
+
+            nativeListView1.Columns.Add("Nombre");
+            nativeListView1.Columns.Add("Sitio");
+
+            nativeListView1.Items.Add("9gag", 0).SubItems.Add("9gag.com");
+            nativeListView1.Items.Add("500px", 1).SubItems.Add("500px.com");
+            nativeListView1.Items.Add("Evernote", 2).SubItems.Add("evernote.com");
+            nativeListView1.Items.Add("Facebook Messenger", 3).SubItems.Add("messenger.com/platform");
+            nativeListView1.Items.Add("Facebook", 4).SubItems.Add("facebook.com");
+            nativeListView1.Items.Add("Firefox", 5).SubItems.Add("mozilla.org/en-US/firefox/new");
+            nativeListView1.Items.Add("Flipboard", 6).SubItems.Add("flipboard.com");
+            nativeListView1.Items.Add("Google", 7).SubItems.Add("google.com");
+            nativeListView1.Items.Add("Google Plus", 8).SubItems.Add("plus.google.com");
+            nativeListView1.Items.Add("Internet Explorer", 9).SubItems.Add("windows.microsoft.com/en-us/internet-explorer/");
+            nativeListView1.Items.Add("Ingress", 10).SubItems.Add("ingress.com");
+            nativeListView1.Items.Add("instagram", 11).SubItems.Add("instagram.com");
+            nativeListView1.Items.Add("Office", 12).SubItems.Add("office.microsoft.com");
+            nativeListView1.Items.Add("Opera", 13).SubItems.Add("opera.com");
+            nativeListView1.Items.Add("Pinterest", 14).SubItems.Add("pinterest.com");
+            nativeListView1.Items.Add("Pocket", 15).SubItems.Add("getpocket.com");
+            nativeListView1.Items.Add("OneDrive", 16).SubItems.Add("onedrive.live.com");
+            nativeListView1.Items.Add("Twitter", 17).SubItems.Add("twitter.com");
+
+            
+            nativeListView1.EndUpdate();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            nativeListView1.View = (View)comboBox1.SelectedValue;
+            nativeListView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+        }
+    }
+}
